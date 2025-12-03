7fb9718c555efb2531025641a809b6157d529716
n0P7xJ <program79766971@gmail.com>
2025-12-03 16:58:45 +0100

    Fix bug

--- src/main/java/com/student/myspringboot/config/SecurityConfig.java
+++ src/main/java/com/student/myspringboot/config/SecurityConfig.java
@@ -25,7 +25,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(Arrays.asList("http://localhost:3000", "http://localhost:5173"));
+        configuration.setAllowedOrigins(Arrays.asList("http://localhost:3000", "http://localhost:5173", "http://localhost:5174"));
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"));
         configuration.setAllowedHeaders(Arrays.asList("*"));
         configuration.setAllowCredentials(true);
